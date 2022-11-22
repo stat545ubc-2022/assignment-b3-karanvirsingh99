02e25ac0f4376f54514b175620b755e6c2d510d2
Karanvir Singh <singh.ghuman.karan@gmail.com>
2022-11-22 15:14:57 -0800

    Updated with comment blocks and better color selection (so that more neighborhoods can be coloured, with repeated colors)

--- app.R
+++ app.R
@@ -5,6 +5,10 @@ library(ggsci)
 
 options(repos = BiocManager::repositories())
 
+#################################################################
+##                         Sample data                         ##
+#################################################################
+
 images <- tibble(n = c(1:4),
                  filename = c("HGS_B_4_B+T_Scan1_Core[2,3,J]_[19600,46803]_component_data.tif",
                               "HGS_B_4_B+T_Scan1_Core[4,2,E]_[14543,54060]_component_data.tif",
@@ -12,23 +16,38 @@ images <- tibble(n = c(1:4),
                               "HGS_D_4_B+T_Scan1_Core[4,4,C]_[14067,54839]_component_data.tif"))
 ihc_data <- read.csv("ihc_data.csv.gz")
 
-ui <- fluidPage(titlePanel("SPIAT package demo"),
+#################################################################
+##                        UI function                          ##
+#################################################################
+
+ui <- fluidPage(titlePanel("SPIAT package demo"), #Title 
                 sidebarLayout(
-  sidebarPanel(selectInput("image", "Select image", choices = setNames(images$filename, images$n)),
-               sliderInput("n_size", "Minimum neighborhood size", value = 12, min = 0, max = 30),
-               sliderInput("radius", "Maximum cell-cell distance", value = 15, min = 0, max = 30),
-               submitButton("Update View", icon("refresh"))),
-  mainPanel(imageOutput("selectedImage"),
-            plotOutput("neighborhood_map", width = 400, height=400))))
+                  #Sidebar Panel
+  sidebarPanel(selectInput("image", "Select image", choices = setNames(images$filename, images$n)), #Image selection
+               sliderInput("n_size", "Minimum neighborhood size", value = 12, min = 0, max = 30), #Neighborhood size input
+               sliderInput("radius", "Maximum cell-cell distance", value = 15, min = 0, max = 30), #Radius input
+               submitButton("Update View", icon("refresh"))), #Update view button
+               #Main Panel
+  mainPanel(imageOutput("selectedImage"), #Selected image output
+            plotOutput("neighborhood_map", width = 400, height=400) # Image output
+  )
+                ))
+
+
+#################################################################
+##                       Server function                       ##
+#################################################################
 
 server <- function(input, output) {
 
+  # Filter sample data based on which image is chosen
   single_image_data <- reactive({
     ihc_data %>% filter(Image == input$image) %>%
     mutate(ID = row_number())
     })
 
 
+  # Create a spatial object based on the sample data from chosen image
   spe_object <- reactive({
     coord_x = single_image_data()$Centroid_x
     coord_y = single_image_data()$Centroid_y
@@ -40,7 +59,7 @@ server <- function(input, output) {
                                    phenotypes = phenotypes, coord_x = coord_x,coord_y = coord_y)
   })
 
-
+# Render image based on which image is chosen
 output$selectedImage <- renderImage({
   list(
     src = file.path("images", paste0(input$image, ".png")),
@@ -50,6 +69,9 @@ output$selectedImage <- renderImage({
     )
   }, deleteFile = FALSE)
 
+# Output neighborhood map based on image chosen, neighborhood radius and minimum neighborhood size
+
+# Step 1 - use the SPIAT::identify_neighborhoods function to create a dataframe that tells me which cells are in which neighborhood
 output$neighborhood_map <- renderPlot({
   plot1 <- identify_neighborhoods(spe_object(),
                                   cell_types_of_interest = c("CD20p", "CD3pCD8p", "CD3pCD8n", "CD79ApCD20n", "CD3pCD8nFoxP3p",
@@ -58,20 +80,27 @@ output$neighborhood_map <- renderPlot({
                                   radius=input$radius,
                                   min_neighborhood_size = input$n_size,
                                   feature_colname = "Phenotype")
-
+  
+  # Step 2 - extract the cell coordinates from the neighborhood output
   coordinates <- data.frame(spatialCoords(plot1)) %>%
     mutate(Cell.ID = row_number())
-
+  
+  # Step 3 - invert y-axis so that it matches the image (just a QuPath coordinates quirk)
   coordinates$Cell.Y.Position <- max(coordinates$Cell.X.Position) - coordinates$Cell.Y.Position
 
+  # Step 4 - extract the phenotypes from the neighborhood output (this also has the neighborhood ID)
   phenotypes <- colData(plot1) %>%
     as.data.frame() %>%
     mutate(Cell.ID = as.numeric(Cell.ID)) %>%
     arrange(Cell.ID)
 
+  # Join coordinates and neighborhood id
   coordinates <- left_join(coordinates, phenotypes)
+  
+  # I want to colour only the cells that are in a neighborhood, so I subset the previous df
   in_neighborhoods <- subset(coordinates, Neighborhood != "Free_cell" & !is.na(Neighborhood))
 
+  # Calculate where to plot the label - this is part of the source code for identify_neighborhood function
   label_location <- vector()
   for (Clusternumber in unique(in_neighborhoods$Neighborhood)) {
     cells_in_Cluster <- in_neighborhoods[in_neighborhoods$Neighborhood ==
@@ -88,17 +117,21 @@ output$neighborhood_map <- renderPlot({
 
   label_location <- as.data.frame(label_location)
   
+  # Clean up the label locations (convert to numeric, add column names)
   label_location[,2:3] <- sapply(label_location[,2:3], as.numeric)
   colnames(label_location) <- c("Cluster", "Cell.X.Position", "Cell.Y.Position")
   label_location$Cluster <- gsub("Cluster_", "", label_location$Cluster)
 
-
+  #Make a colour vector (some colours are repeated but that is A-okay!)
+  colors = rep(ggsci::pal_npg()(10), 100)[1:length(label_location$Cluster)]
+  
+  # Plot the cells, colored by neighborhood with text label with neighborhood ID
   ggplot(coordinates, aes(Cell.X.Position, Cell.Y.Position))+
     geom_point(alpha = 0.2)+
     geom_point(data=in_neighborhoods, aes(color=Neighborhood))+
     geom_text(data=label_location, aes(x=Cell.X.Position, y=Cell.Y.Position,
                                        label=Cluster), size=14)+
-    scale_color_npg()+
+    scale_color_manual(values=colors)+
     theme_void()+
     theme(legend.position="none")
 })
